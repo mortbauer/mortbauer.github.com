f8c28f81986447ff307560ef5561d1004949611e
Martin Ortbauer <mortbauer@gmail.com>
2012-02-01 09:42:58 +0200

    good start for now

--- src/fabfile.py
+++ src/fabfile.py
@@ -2,11 +2,6 @@ from fabric.api import *
 import os
 import fabric.contrib.project as project
 
-PROD = 'sl'
-DEST_PATH = '..'
-ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
-DEPLOY_PATH = os.path.join(ROOT_PATH, 'deploy')
-
 def clean():
     local('rm -rf ./deploy')
 
@@ -23,15 +18,3 @@ def serve():
 def reserve():
     regen()
     serve()
-
-def smush():
-    local('smusher content/media/images/')
-
-@hosts(PROD)
-def publish():
-    regen()
-    project.rsync_project(
-        remote_dir=DEST_PATH,
-        local_dir=DEPLOY_PATH.rstrip('/') + '/',
-        delete=True
-    )
