00c902054920c1e2ac1024a44f282bf725b29c3f
Martin Ortbauer <mortbauer@gmail.com>
2012-02-26 10:03:47 +0200

    new blog tampere, galleria used

--- src/extensions/miscellaneous.py
+++ src/extensions/miscellaneous.py
@@ -7,20 +7,13 @@ from jinja2 import contextfunction
 
 
 @contextfunction
-def listdir(context, path):
+def listimagedir(context, path):
     """
-    Returns a list of all files in the given partial path.
+    Returns a list of all files in the given partial imagepath.
     """
-    #return  os.listdir(context['site'].content_url(path))
-    #return  os.listdir(context['site'].media_url(path))
-    list = os.listdir(path)
-    #return [os.path.join(root,i) for i in list]
-    return [context['site'].media_url('images/'+i) for i in list]
-    #return os.listdir('.')
-    #return list
-    #return path
+    list = os.listdir('content/media/images/'+path)
+    return [context['site'].media_url('images/'+path+i) for i in list]
 
 class Miscellaneous(Plugin):
-
     def template_loaded(self, template):
-            template.env.globals['listdir'] = listdir
+            template.env.globals['listimagedir'] = listimagedir
